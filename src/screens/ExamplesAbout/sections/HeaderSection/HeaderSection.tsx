d7ab66796e6cde87c6ec497c821ca3aa22c7ced1
JsMelix <juansmelo03@gmail.com>
2025-03-17 15:45:32 -0500

    logos

--- src/screens/ExamplesAbout/sections/HeaderSection/HeaderSection.tsx
+++ src/screens/ExamplesAbout/sections/HeaderSection/HeaderSection.tsx
@@ -26,7 +26,7 @@ export const HeaderSection = (): JSX.Element => {
         <img 
           className="h-10 w-auto object-contain" 
           alt="AI Young Hub Logo" 
-          src="public/ai-logo.jpg" 
+          src="public/ai-logo.svg" 
         />
       </div>
 
